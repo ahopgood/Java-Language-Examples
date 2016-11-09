b1ce8c46eea19b3c9528b3737f724c8a945e4e44
ahopgood <alex.hopgood@gmail.com>
2016-11-09 19:46:48 +0000

    Added tests for the FileNavigationHelper class which demonstrates the uses of the Paths class and Path interface

--- Java7/src/main/java/com/alexander/java/examples/java7/files/FileNavigationHelper.java
+++ Java7/src/main/java/com/alexander/java/examples/java7/files/FileNavigationHelper.java
@@ -21,29 +21,55 @@ public class FileNavigationHelper {
      * on the filesystem, use for doing stuff with files.
      */
 
-    public FileNavigationHelper(){
-        File workingDirectory = new File("");
-        String baseDir = workingDirectory.getAbsolutePath();
+    public static File workingDirectory = new File("");
+    public static String baseDir = workingDirectory.getAbsolutePath();
+    public static Path parentDirectory = Paths.get(baseDir, "src", "main", "java", "com", "alexander", "examples", "java7");
 
-        //Reference to a file
-        Path src = Paths.get(baseDir, "src", "main", "java", "readme.txt");
+    //Reference to a file
+    public static Path getFileReference(){
+        System.out.println("Finding file");
+        System.out.println(parentDirectory);
+        Path src = Paths.get(parentDirectory.toString(), "readme.txt");
+        System.out.println(src);
+        return src;
+    }
 
+    public static Path getDirectoryReference(){
         //Reference to a path
-        Path directory = Paths.get(baseDir, "src", "main", "java");
+        System.out.println("Finding directory");
+        Path directory = Paths.get(parentDirectory.toString());
+        System.out.println(directory);
+        return directory;
+    }
 
-        Path parentDirectory = Paths.get(baseDir, "src", "main", "java", "com", "alexander", "examples", "java7");
-
-        //Path navigation
+    //Path navigation
+    public static Path getChildReference(){
         //resolve a child path
+        System.out.println("Finding child");
+        System.out.println(parentDirectory);
         Path filesDir = parentDirectory.resolve("files");
         System.out.println(filesDir);
-
-        //resolve the testdir sibling found in the examples parent dir
-        Path testdir = parentDirectory.resolveSibling("testdir");
-        System.out.println(testdir.toAbsolutePath());
-
-        //Create a relative path
-        Path parentRelativeToTestDir = testdir.relativize(parentDirectory);
-        System.out.println(parentRelativeToTestDir.toString());
+        return filesDir;
     }
+
+    //Path navigation
+    public static Path getSiblingReference(){
+        //resolve the testdir sibling found in the examples parent dir
+        System.out.println("Finding sibling");
+        System.out.println(parentDirectory.toString());
+        Path testdir = parentDirectory.resolveSibling("testdir");
+        System.out.println(testdir);
+        return testdir;
+    }
+
+    public static Path getRelativeDirectory(){
+        //Create a relative path
+        System.out.println("Finding testdir relative to parent");
+        System.out.println(parentDirectory);
+        Path testdir = Paths.get(parentDirectory.toString(), "testdir");
+        Path parentRelativeToTestDir = testdir.relativize(parentDirectory);
+        System.out.println(parentRelativeToTestDir);
+        return parentRelativeToTestDir;
+    }
+
 }
--- Java7/src/test/java/com/alexander/java/examples/java7/files/FileNavigationHelperTest.java
+++ Java7/src/test/java/com/alexander/java/examples/java7/files/FileNavigationHelperTest.java
@@ -9,9 +9,30 @@ import static org.junit.Assert.*;
  */
 public class FileNavigationHelperTest {
 
+
+    String separator = System.getProperty("file.separator");
+    String parent = FileNavigationHelper.parentDirectory.toString();
+
     @Test
     public void testFileReference(){
-        FileNavigationHelper helper = new FileNavigationHelper();
+        assertEquals(parent+separator+"readme.txt",
+                FileNavigationHelper.getFileReference().toString());
+    }
+    @Test public void testDirectoryReference(){
+        assertEquals(parent,
+                FileNavigationHelper.getDirectoryReference().toString());
+    }
+    @Test public void childReference(){
+        assertEquals(parent+separator+"files",
+                FileNavigationHelper.getChildReference().toString());
+    }
+    @Test public void testSiblingReference(){
+        assertEquals(parent.replace("java7","testdir"),
+                FileNavigationHelper.getSiblingReference().toString());
+    }
+    @Test public void testRelativeDirectory(){
+        assertEquals("..", //as the test directory is one up in the dir structure
+                FileNavigationHelper.getRelativeDirectory().toString());
     }
 
 }
