f4e0f7dae358cd04b71a0db71076afc16e8e5c63
ahopgood <alex.hopgood@gmail.com>
2017-01-06 16:21:41 +0000

    Baffling phaser example, need to unpick to understand more.

---
+++ Java7/src/main/java/com/alexander/java/examples/java7/concurrency/PhaserUsage.java
@@ -0,0 +1,57 @@
+package com.alexander.java.examples.java7.concurrency;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Phaser;
+
+/**
+ * Demonstration of a {@link Phaser} which is a reusable synchronization barrier.
+ *
+ * Tasks register with the Phaser.
+ * Tasks arrive at a particular phase within the phaser's sequence of phases, this phase number is important.
+ * Tasks then wait for the rest of tasks to arrive at the phaser, tasks can be registered to activate on a particular phase.
+ * The phaser advances to the next phase after all tasks arrive for a particular phase.
+ *
+ * Created by alexhopgood on 05/01/17.
+ */
+public class PhaserUsage {
+
+    public static void main(String[] args){
+        PhaserUsage usage = new PhaserUsage();
+        System.out.println(System.currentTimeMillis());
+        List<Runnable> tasks = new LinkedList<Runnable>();
+        for (int i = 0; i < 25; i++){
+            tasks.add(new PhaserUsage().new Printer(i));
+        }
+        usage.runTasks(tasks);
+        System.out.println(System.currentTimeMillis());
+    }
+
+    void runTasks(List<Runnable> tasks) {
+        final Phaser phaser = new Phaser(1); // "1" to register self
+        // create and start threads
+        for (final Runnable task : tasks) {
+            phaser.register();
+            new Thread() {
+                public void run() {
+                    phaser.arriveAndAwaitAdvance(); // await all creation
+                    task.run();
+                }
+            }.start();
+        }
+
+        // allow threads to start and deregister self
+        phaser.arriveAndDeregister();
+    }
+
+    public class Printer implements Runnable{
+        int count = 0;
+        public Printer(int i){
+            count = i;
+        }
+        @Override
+        public void run() {
+            System.out.println(count+" "+System.currentTimeMillis());
+        }
+    }
+}
--- Java7/src/main/java/com/alexander/java/examples/java7/files/FileSystemUsage.java
+++ Java7/src/main/java/com/alexander/java/examples/java7/files/FileSystemUsage.java
@@ -18,8 +18,8 @@ import java.util.Set;
  * {@link java.nio.file.FileSystems} is a factory for the {@link FileSystem} (interface)
  * One can create a custom filesystem by implementing {@link java.nio.file.spi.FileSystemProvider} for URI, caching and file
  * handling and by implementing {@link FileSystem} for Roots, Read-Write and file store.
- * {@link https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemproviderprops.html} provides a list
- * of the environmental properties available for the {@link https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemprovider.html}.
+ * <a href="https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemproviderprops.html">Zip Filesystem Provider Properties</a> provides a list
+ * of the environmental properties available for the <a href="https://docs.oracle.com/javase/7/docs/technotes/guides/io/fsp/zipfilesystemprovider.html">Zip Filesystem Provider</a></a>.
  * Created by alexhopgood on 05/12/16.
  */
 public class FileSystemUsage {
