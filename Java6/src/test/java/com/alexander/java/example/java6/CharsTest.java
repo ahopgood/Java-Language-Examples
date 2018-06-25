672528170c2a788c8b7f3c1883553b8c3cb7e79a
Alexander Hopgood <alex.hopgood@gmail.com>
2018-06-25 21:29:22 +0100

    Unicode 6 example.

---
+++ Java6/src/test/java/com/alexander/java/example/java6/CharsTest.java
@@ -0,0 +1,27 @@
+package com.alexander.java.example.java6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CharsTest {
+    @Test
+    public void testUnicode6(){
+        char start = '\u0000';
+        System.out.println("["+start+"]");
+        char end = '\uFFFF';
+        System.out.println("["+end+"]");
+
+        int emojiCodePoint = 0x1F601;
+
+        System.out.println(Character.charCount(emojiCodePoint));
+        System.out.println("["+Character.toChars(emojiCodePoint)[0] + Character.toChars(emojiCodePoint)[1]+"]");
+
+        String description = "GRINNING FACE WITH SMILING EYES";
+//        assertEquals(description, Character.getName(emojiCodePoint));
+        //Contains both a
+        assertEquals(2, Character.charCount(emojiCodePoint));
+
+        assertEquals("😁", new String(new char[]{Character.toChars(emojiCodePoint)[0], Character.toChars(emojiCodePoint)[1]}));
+    }
+}
