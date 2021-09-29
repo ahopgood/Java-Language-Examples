ff627ec59cd7e1ea2420d45bc6408a2eddd424ea
Alex Hopgood <alex.hopgood@gmail.com>
2021-09-29 12:05:33 +0100

    Adding legacy random example

--- Java17/src/test/java/PseudoRandomTest.java
+++ Java17/src/test/java/PseudoRandomTest.java
@@ -6,8 +6,15 @@ import java.util.random.RandomGeneratorFactory;
 public class PseudoRandomTest {
 
     @Test
-    void test() {
+    void test_LXM_random() {
         RandomGeneratorFactory<RandomGenerator> generatorFactory = RandomGeneratorFactory.of("L32X64MixRandom");
+        RandomGenerator generator = generatorFactory.create();
+        System.out.println(generator.nextInt());
+    }
+
+    @Test
+    void testLegacy_SecureRandom() {
+        RandomGeneratorFactory<RandomGenerator> generatorFactory = RandomGeneratorFactory.of("SecureRandom");
         RandomGenerator generator = generatorFactory.create();
         System.out.println(generator.nextInt());
     }
