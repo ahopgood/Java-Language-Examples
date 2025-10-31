18c35704f1d38fa03562d86681a975452357df1c
Alex Hopgood <alex.hopgood@askattest.com>
2025-10-31 10:44:19 +0000

    Adding more collector examples

--- Java8/src/test/java/com/alexander/java/example/java8/streams/CollectTest.java
+++ Java8/src/test/java/com/alexander/java/example/java8/streams/CollectTest.java
@@ -3,9 +3,11 @@ package com.alexander.java.example.java8.streams;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -45,5 +47,53 @@ public class CollectTest {
                 .filter(x -> x > 4)
                 .collect(toMap(Function.identity(), i -> "" + i));
         assertEquals(5, mapUsingIdentity.size());
+    }
+
+
+
+    @Test
+    public void testCollectGroupingBy() {
+        List<Artist> artists = new LinkedList<>();
+
+        Artist theBeatles = new Artist().setFrom("Liverpool").setName("The Beatles");
+        Artist theProdigy = new Artist().setFrom("Braintree").setName("The Prodigy");
+        Artist sunsOfThunder = new Artist().setFrom("Neath").setName("Suns of Thunder");
+        Artist oasis = new Artist().setFrom("Manchester").setName("Oasis");
+        Artist theCoral = new Artist().setFrom("liverpool").setName("The Coral");
+
+        artists.add(theBeatles);
+        artists.add(theProdigy);
+        artists.add(sunsOfThunder);
+        artists.add(oasis);
+        artists.add(theCoral);
+
+        Map<String, Long> map = artists.stream().collect(
+                Collectors.groupingBy(artist -> artist.getName(), Collectors.counting())
+        );
+
+        System.out.println(map);
+    }
+
+    @Test
+    public void testCollectPartitionBy() {
+        List<Artist> artists = new LinkedList<>();
+
+        Artist theBeatles = new Artist().setFrom("Liverpool").setName("The Beatles");
+        Artist theProdigy = new Artist().setFrom("Braintree").setName("The Prodigy");
+        Artist sunsOfThunder = new Artist().setFrom("Neath").setName("Suns of Thunder");
+        Artist oasis = new Artist().setFrom("Manchester").setName("Oasis");
+        Artist theCoral = new Artist().setFrom("liverpool").setName("The Coral");
+
+        artists.add(theBeatles);
+        artists.add(theProdigy);
+        artists.add(sunsOfThunder);
+        artists.add(oasis);
+        artists.add(theCoral);
+
+        Map<Boolean, List<Artist>> map = artists.stream().collect(
+                Collectors.partitioningBy(artist -> artist.isFrom("liverpool"))
+        );
+
+        System.out.println(map);
     }
 }
